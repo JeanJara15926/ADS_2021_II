49825a077626642b7e4a1b9be0ed3808c1de7289
GianinaMoya <2015200023@untels.edu.pe>
2021-12-24 01:48:05 -0500

    publis

--- moduloGestionarUsuarios/formGestionarUsuarios.php
+++ moduloGestionarUsuarios/formGestionarUsuarios.php
@@ -1,2 +1,88 @@
->?
->
+<?php
+    include_once("..../shared/formulario.php");
+    class formGestionarUsuarios extends formulario{
+        public function formGestionarUsuariosShow(){
+            $this -> encabezadoShowSimple("Gestionar Usuarios ♂♀");
+            ?>
+            
+            <div class="container usuarios">
+                <div class="row">                    
+                    <div class="col-10">
+                        <p><i class='fas fa-user-alt'></i><?php echo $_SESSION['login'] ?>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="container">
+                <form method="post" action="#" enctype="multipart/form-data">
+                    <div class="">
+                        <h1 class="text-center">REGISTRO USUARIOS</h1>
+                        <div class="mb-3 row">
+                            <label for="texto_IdUsuario" class="col-sm-2 col-form-label">ID USUARIO</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="texto_IdUsuario">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="texto_Nombre" class="col-sm-2 col-form-label">NOMBRE</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="texto_Nombre">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="texto_DNI" class="col-sm-2 col-form-label">DNI</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="texto_DNI">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="texto_Rol" class="col-sm-2 col-form-label">ROL</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="texto_Rol">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="texto_Login" class="col-sm-2 col-form-label">LOGIN</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="texto_Login">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="texto_Password" class="col-sm-2 col-form-label">PASSWORD</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="texto_Password">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="texto_Estado" class="col-sm-2 col-form-label">ESTADO</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="texto_Estado">
+                            </div>
+                        </div>
+
+
+                        <div class="mb-3 row">
+                            <div class="col-5"></div>
+                            <div class="col">                                
+                                <input type="button" class="btn btn-primary Btn_CrearUsuario" value="Guardar" onclick="CrearUsuario()"/>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+            <?php
+        }
+    }
+?>
+
+        
