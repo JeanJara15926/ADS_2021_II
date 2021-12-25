2f83c03adef9c3b7c46dbe3328383421430bea7f
Jean Pierre <jeanjara.15926@gmail.com>
2021-12-25 14:02:41 -0500

    UC cambiar Contraseña

--- moduloSeguridad/getUsuario.php
+++ moduloSeguridad/getUsuario.php
@@ -1,9 +1,10 @@
 <?php
 if(isset($_POST['btnEnviar'])){
+    
     $login = trim(strtolower($_POST['login']));
     $password = trim($_POST['password']);
-    $resultado_validacion_campos= validarCampos($login,$password);
-    if($resultado_validacion_campos==0){
+    $validaCampos= validarCampos($login,$password);
+    if($validaCampos==0){
         include_once("../shared/formMensajeSistema.php");
         $objMensaje = new formMensajeSistema;
         $objMensaje -> formMensajeSistemaShow("Los datos ingresados no son válidos para cotejo","<a href='../index.php'>Intente nuevamente</a>");
@@ -13,6 +14,7 @@ if(isset($_POST['btnEnviar'])){
         $objController -> ValidarUsuario($login,$password);
     }
 }else if(isset($_POST['retrocede'])){
+
     session_start();
     include_once("../model/usuarioPrivilegio.php");
     include_once("controllerAutenticarUsuario.php");
@@ -25,17 +27,53 @@ if(isset($_POST['btnEnviar'])){
     include_once("formMenuPrincipal.php");
     $objMenuPrincipal = new formMenuPrincipal;
     $objMenuPrincipal -> formMenuPrincipalShow($listaPrivilegios);
-}
-function ComprobarCampo($login){
-    if(strlen($login)>3  ){
+
+}else if(isset($_POST['btnContrasena'])){
+
+    $ncontra = trim($_POST['ncontra']);
+    $acontra = trim($_POST['acontra']);
+    $ccontra = trim($_POST['ccontra']);
+    $login = $_POST['loginc'];
+    $validacion_contras= validarContras($ncontra,$acontra,$ccontra);
+    $igual_contras= validarIgualdad($ncontra,$ccontra);
+    $dirigir = "<form method='POST' action='indexCambiarPassword.php'>
+                        <input type='submit' class='btnTacza' name='bntOk' value='Volver' />
+                </form>";
+    if($igual_contras==0){
+        include_once("../shared/formMensajeSistema.php");
+        $objMensaje = new formMensajeSistema;        
+        $objMensaje -> formMensajeSistemaShow("Los campos no son coinciden",$dirigir);
+    }else if($validacion_contras==0){
+        include_once("../shared/formMensajeSistema.php");
+        $objMensaje = new formMensajeSistema;
+        $objMensaje -> formMensajeSistemaShow("Los datos ingresados no son válidos para cotejo",$dirigir);
+    }else{
+        include_once("controllerCambiarContrasena.php");
+        $objControllerContra = new controllerCambiarContrasena;
+        $objControllerContra -> ValidarControllerContras($ncontra,$acontra,$login);
+    }
+} 
+
+function validarCampos($login,$password){
+    if(strlen($login)>3 and strlen($password)>3){
         return (1);
     }
     else{
         return (0);
     }
 }
-function validarCampos($login,$password){
-    if(strlen($login)>3 and strlen($password)>3){
+
+function validarContras($ncontra,$acontra,$ccontra){
+    if(strlen($ncontra)>0 and strlen($acontra)>0 and strlen($ccontra)>0){
+        return (1);
+    }
+    else{
+        return (0);
+    }
+}
+
+function validarIgualdad($ncontra,$ccontra){
+    if($ncontra == $ccontra){
         return (1);
     }
     else{
