71cd3c9608ec166be639d2f2b4b9c6933daa8c68
GianinaMoya <2015200023@untels.edu.pe>
2021-12-25 12:18:23 -0500

    Actualizado Gianina

--- moduloGestionarUsuarios/controllerGestionarUsuario.php
+++ moduloGestionarUsuarios/controllerGestionarUsuario.php
@@ -1,27 +1,30 @@
 <?php
-class controllerGestionarUsuarios{
-// Moya
-    public function insertarUsuario($idusuario,$nombre,$dni,$rol,$login,$password,$estado){
+class controllerGestionarUsuario{
+
+    public function insertarUsuario($idUsuario,$nombre,$dni,$rol,$login,$password,$estado){
         include_once("../model/usuario.php");
-        $objUsuario = new Usuario;
-        $resultado=$objUsuario->insertarUsuario($idusuario,$nombre,$dni,$rol,$login,$password,$estado);
-        if($resultado=0){
+        $objUsuario = new usuario;
+        $anwer=$objUsuario->insertarUsuario($idUsuario,$nombre,$dni,$rol,$login,$password,$estado);
+        if($answer=0){
             echo "Error al insertar Usuario";
         }else{
             echo "1";
         }
     }
 
+    
+
+
     //Extraer Usuario
     public function ExtraerUsuarios(){
         include_once("../model/usuario.php");
-        $objUsuario_ii = new Usuario;
+        $objUsuario_ii = new usuario;
         $ListaUsuarios = $objUsuario_ii->ExtraerUsuarios();
         $filas=count($ListaUsuarios);
         $arr=array();
         for($i=0;$i<$filas;$i++){
             $arr[$i] = array(
-                'idusuario' => $ListaUsuarios[$i]['idusuario'],
+                'idUsuario' => $ListaUsuarios[$i]['idUsuario'],
                 'nombre' => $ListaUsuarios[$i]['nombre'], 
                 'dni' => $ListaUsuarios[$i]['dni'],
                 'rol' => $ListaUsuarios[$i]['rol'],
@@ -35,10 +38,10 @@ class controllerGestionarUsuarios{
 
     public function BuscarUsuarioEditar($id_editar){
         include_once("../model/usuario.php");
-        $objUsuario_ii = new Usuario;
+        $objUsuario_ii = new usuario;
         $Usuario = $objUsuario_ii->BuscarUsuarioEditar($id_editar);
         $arr =  array(
-            'idusuario' => $Usuario[0]['idusuario'],
+            'idUsuario' => $Usuario[0]['idUsuario'],
             'nombre' => $Usuario[0]['nombre'], 
             'dni' => $Usuario[0]['dni'],
             'rol' => $Usuario[0]['rol'],
@@ -49,21 +52,22 @@ class controllerGestionarUsuarios{
         echo json_encode($arr);
     }
 
-    public function EliminarUsuario($idusuario){
+    public function EliminarUsuario($idUsuario){
         include_once("../model/usuario.php");
-        $objProducto_ii = new Usuario;
-        $objProducto_ii->EliminarUsuario($idusuario);
+        $objUsuario_ii = new usuario;
+        $objUsuario_ii-> EliminarUsuario($idUsuario);
     }
 
+    
     public function BuscarUsuario($nombre){
         include_once("../model/usuario.php");
-        $objUsuario_ii = new Usuario;
+        $objUsuario_ii = new usuario;
         $ListaUsuarios = $objUsuario_ii->BuscarUsuarioNombre($nombre);
         $filas=count($ListaUsuarios);
         $arr=array();
         for($i=0;$i<$filas;$i++){
             $arr[$i] = array(
-                'idusuario' => $ListaUsuarios[$i]['idusuario'],
+                'idUsuario' => $ListaUsuarios[$i]['idUsuario'],
                 'nombre' => $ListaUsuarios[$i]['nombre'], 
                 'dni' => $ListaUsuarios[$i]['dni'],
                 'rol' => $ListaUsuarios[$i]['rol'],
@@ -75,12 +79,12 @@ class controllerGestionarUsuarios{
         echo json_encode($arr);
     }
 
-    public function modificarUsuario($id_EditarChambicito,$idusuario,$nombre,$dni,$rol,$login,$password,$estado){
+    public function modificarUsuario($id_EditarChambicito,$idUsuario,$nombre,$dni,$rol,$login,$password,$estado){
         include_once("../model/usuario.php");               
-        $objProducto = new Producto;
-        $objProducto->modificarProducto($idEditarChambicito,$producto,$precio, $stock,$imagen);
+        $objUsuario = new usuario;
+        $objProducto->modificarUsuario($id_EditarChambicito,$idUsuario,$nombre,$dni,$rol,$login,$password,$estado);
         if($retorno=0){
-            echo "Error al actualizar producto";
+            echo "Error al actualizar Usuario";
         }else{
             echo "1";            
         }
