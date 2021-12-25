b55439d605602d3dce5847ffb5bf5c07f6fac6a4
Jean Pierre <jeanjara.15926@gmail.com>
2021-12-25 18:46:43 -0500

    reset

---
+++ model/conexion.php
@@ -0,0 +1,15 @@
+<?php
+    class conexion{
+        protected function conectar(){
+            //$conn= mysqli_connect("localhost", "root", "", "tienda_chambi");
+           // $conn= mysqli_connect("localhost:3307", "root", "", "tienda_edmar");
+           $conn= mysqli_connect("localhost", "root", "123456789", "tienda_edmar2");
+            return $conn;
+            
+        }
+        protected function desconectar($conectados){
+            mysqli_close($conectados);            
+            //echo "me desconecte :(";
+        }
+    } 
+ ?>
