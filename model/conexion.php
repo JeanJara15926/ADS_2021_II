1095981c4f7cc75d66b8e1b1cc392d3ee386dcfe
Jean Pierre <jeanjara.15926@gmail.com>
2021-12-25 18:15:54 -0500

    ss

--- model/conexion.php
+++ model/conexion.php
@@ -3,8 +3,9 @@
         protected function conectar(){
             //$conn= mysqli_connect("localhost", "root", "", "tienda_chambi");
            // $conn= mysqli_connect("localhost", "root", "123456789", "tienda_edmar2");
-           $conn= mysqli_connect("localhost:3307", "root", "", "tienda_edmar");
+           $conn= mysqli_connect("localhost", "root", "123456789", "tienda_edmar2");
             return $conn;
+            ///asdasdasd
         }
         protected function desconectar($conectados){
             mysqli_close($conectados);            
